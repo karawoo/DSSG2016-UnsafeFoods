dc53863c2f9f1dcfd8890900458874b32fad013e
Kara Woo <woo.kara@gmail.com>
2016-07-28 13:35:23 -0700

    Plot documents after DNMF

--- code/discriminant-nmf.R
+++ code/discriminant-nmf.R
@@ -7,6 +7,8 @@ library("jsonlite")
 library("DNMF")
 library("tm")
 library("dplyr")
+library("tidyr")
+library("ggplot2")
 
 ## Load Amazon review data
 json_file <- "../data/raw/reviews_Grocery_and_Gourmet_Food.json"
@@ -56,3 +58,38 @@ trainlabel <- amz_sub$recalled
 
 ## Run DNMF
 dnmf_output <- DNMF(tdm, trainlabel)
+
+#################
+####  Plots  ####
+#################
+
+## Plot documents (H matrix) to see if the points fall neatly into two clusters
+## (i.e. a recalled and not-recalled cluster) -- per discussion with Valentina
+## on 28 July.
+
+## Columns in H matrix are documents; rows are weights of document relative to
+## classes (1 = recalled, 2 = not recalled)
+colnames(dnmf_output$H) <- trainlabel
+rownames(dnmf_output$H) <- c(1, 2)
+
+## Create data frame where each row is a document. orig_class column should have
+## the original class (1 or 2 for recalled/not recalled). Columns "val1" and
+## "val2" should be the weights of document relative to each class.
+h <- data.frame(dnmf_output$H) %>%
+  gather(x, value) %>%
+  mutate(orig_class = rep(trainlabel, each = 2),
+         class = rep(c("val1", "val2"), nrow(.) / 2)) %>%
+  spread(class, value)
+
+## Plot val1 and val2 against each other and color points by original document
+## class to see if points fall into two groups
+ggplot(h, aes(x = val1, y = val2, color = as.factor(orig_class))) +
+  geom_point() +
+  scale_x_log10() +
+  scale_y_log10() +
+  theme(legend.position = "bottom") +
+  labs(x = "Weight in class 1 (recalled)",
+       y = "Weight in class 2 (not recalled)",
+       color = "Original document class")
+
+## Somthing is off here...
